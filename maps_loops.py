0efe7899c87001aa9dc18f5face9a0dff2a07bad
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-10-29 17:37:47 -0300

    Update maps_loops.py

--- maps_loops.py
+++ maps_loops.py
@@ -1,17 +1,17 @@
-def find_max(dict):
+def find_max_value(dictionary):
     a = ""
-    max = 0
-    for key, value in dict.items():
-        if value > max:
-            max = value
+    max_val = None
+    for key, value in dictionary.items():
+        if max_val is None or value > max_val:
+            max_val = value
             a = key
     return a
-
-def reverse_dict(dict):
+    
+def reverse_dict(dictionary):
     a = {}
-    for key, value in dict.items():
+    for key, value in dictionary.items():
         if value in a:
-            a[value]+=k
+            a[value] += key  
         else:
             a[value] = key
     return a
@@ -28,25 +28,29 @@ def word_freq_counter(words):
 
     return frequency
 
-def find_biggest_expense(dict):
+def find_biggest_expense(dictionary):
     max_avg = 0
     max_expense = ''
     
-    for category, costs in expenses.items():
-        average = sum(costs) / len(costs)
-        
-        if average > max_avg:
-            max_avg = average
-            max_expense = category
-            
-    return max_expense    
+    for category, costs in dictionary.items():
+        if costs:  # avoid division by zero
+            average = sum(costs) / len(costs)
+            if average > max_avg:
+                max_avg = average
+                max_expense = category
+                
+    return max_expense
 
-def sum_of_expenses(expenses):
-    total_expenses = {}
+def sum_of_expenses_by_type(expenses):
+    res = {}
     for category, costs in expenses.items():
-        total_expenses[category] = sum(costs)
-    return total_expenses
-
+        for item_type, amount in costs:
+            if item_type in res:
+                res[item_type] += amount
+            else:
+                res[item_type] = amount
+    return res
+    
 def sum_of_expenses_by_type(expenses):
     res = {}
     for k, v in expenses.items():
