7d83a81604701b73944d17b340de6db64f58c726
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-10-29 18:08:01 -0300

    Update set_loops.py

--- set_loops.py
+++ set_loops.py
@@ -1,6 +1,2 @@
-def unique_strings(words):
-    unique_chars = []  
-    for char in words:
-        if char not in unique_chars:  
-            unique_chars.append(char)
-    return unique_chars 
+def unique_strings(ex):
+	return set(ex)
